47f420d8e07e90eeedd058a97127a23d21ffceaf
HrQHm <henrique_hmk@hotmail.com>
2022-04-10 10:08:32 -0300

    Exercício Finalizado

--- src/modules/games/entities/Game.ts
+++ src/modules/games/entities/Game.ts
@@ -2,6 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
   ManyToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,27 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: { 
+        id:user_id 
+      }
+    });
+    
+    if(!user){
+      throw new Error("User not found!");
+    }
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM USERS ORDER BY FIRST_NAME ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM USERS WHERE FIRST_NAME ILIKE $1 AND LAST_NAME ILIKE $2", [`${first_name}`, `${last_name}`]); // Complete usando raw query
   }
 }
